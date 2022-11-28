792f7b0a1f6aca0faca9d3698a8d7ebbbaf2c4b6
muhsin <muhsin.ma.mohamed@gmail.com>
2022-11-28 12:55:31 +0000

    Add constraints to form

--- things/forms.py
+++ things/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from .models import Thing
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 """Forms of the project."""
 
 # Create your forms here.
 class ThingForm(forms.Form):
-    name = forms.CharField()
-    description = forms.CharField(widget=forms.Textarea())
-    quantity = forms.IntegerField(widget=forms.NumberInput())
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(widget=forms.Textarea(), max_length=120)
+    quantity = forms.IntegerField(widget=forms.NumberInput(), validators=[MinValueValidator(0),MaxValueValidator(50)])
