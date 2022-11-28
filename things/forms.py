7b4401faf04d0648e6ae9f52ce2b68190e32abcb
muhsin <muhsin.ma.mohamed@gmail.com>
2022-11-28 12:35:28 +0000

    Add thing form to view and home page template

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,8 @@
+from django import forms
 """Forms of the project."""
 
 # Create your forms here.
+class ThingsForm(forms.Form):
+    name = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea())
+    quantity = forms.IntegerField(widget=forms.NumberInput())
--- things/views.py
+++ things/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ThingsForm
+from .models import Thing
 
 def home(request):
+    form = ThingsForm()
+    if(request.method == 'POST'):
+        form = ThingsForm(request.POST)
+        if(form.is_valid()):
+            name = form.cleaned_data.get('name')
+            description = form.cleaned_data.get('description')
+            quantity = form.cleaned_data.get('quantity')
+            newthing = Thing.objects.create(
+                name=name,
+                description=description,
+                quantity=quantity
+            )
+            newthing.save()
+            return redirect('home')
     return render(request, 'home.html', {'form': form})
